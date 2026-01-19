afc18cc8a745042684f6fccefb674b6b97cbf992
joorgeeml_13 <jorge29364767@gmail.com>
2026-01-19 14:20:12 +0100

    aaaaa

--- ActvLogIn/src/main/java/matias/jorge/actvlogin/controllers/singUp/SingUpController.java
+++ ActvLogIn/src/main/java/matias/jorge/actvlogin/controllers/singUp/SingUpController.java
@@ -1,9 +1,39 @@
 package matias.jorge.actvlogin.controllers.singUp;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 /**
  *
  * @author alumno
  */
 public class SingUpController {
     
+    @FXML
+    private TextField singUpTfPassword;
+    
+    @FXML
+    private TextField singUpTfVerfPassword;
+    
+    @FXML
+    private PasswordField singUpPassworField;
+    
+    @FXML
+    private PasswordField singUpVerfPassword;
+    
+    @FXML
+    public void initialize(){
+        singUpTfPassword.textProperty().bindBidirectional(singUpPassworField.textProperty());
+        singUpTfVerfPassword.textProperty().bindBidirectional(singUpVerfPassword.textProperty());
+        
+        singUpTfPassword.managedProperty().bind(singUpTfPassword.visibleProperty());
+        singUpPassworField.managedProperty().bind(singUpPassworField.visibleProperty());
+        
+        singUpTfVerfPassword.managedProperty().bind(singUpTfVerfPassword.visibleProperty());
+        singUpVerfPassword.managedProperty().bind(singUpVerfPassword.visibleProperty());
+        
+        singUpTfVerfPassword.setVisible(false);
+        singUpTfPassword.setVisible(false);
+    }
 }
